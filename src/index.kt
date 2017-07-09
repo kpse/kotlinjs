277365082c31a608a3f7bfa5a1ee549a1ccc3691
Suo Qin <kpsesuo@gmail.com>
2017-07-09 10:02:38 +1000

    print to DOM

--- src/index.kt
+++ src/index.kt
@@ -1,3 +1,17 @@
+import org.w3c.dom.HTMLElement
+import kotlin.browser.document
+
 fun main(args: Array<String>) {
-    println("hello kotlin/javascript world!")
+    val message = "hello kotlin/javascript world!"
+    println(message)
+    printToDom(message)
+    printToDom(message)
+    printToDom(message)
+    printToDom(message)
+    printToDom(message)
+}
+
+private fun printToDom(message: String) {
+    val content = document.getElementById("content") as HTMLElement
+    content.textContent = "${content.textContent} $message"
 }
